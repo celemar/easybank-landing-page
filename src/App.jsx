ed576be414df8df3fe5a07db29394be67ff21dac
celemar <celemar@pm.me>
2024-05-05 11:15:49 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -85,8 +85,8 @@ function App() {
             <h1>Next generation digital banking</h1>
             <p>
               Take your financial life online. Your Easybank account will be a
-              one-stop-shop for spending, saving, budgeting, investing, and much
-              more.
+              one-stop-shop for spending, saving, 
+              budgeting, investing, and much more.
             </p>
             <button class="request-button">Request Invite</button>
           </div>
