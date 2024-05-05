965d750ba85dda0c62dfa4a4aec5845e0d843864
celemar <celemar@pm.me>
2024-05-05 10:28:52 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,9 @@ function App() {
   const leftItems = footerItems.slice(0, 3);
   const rightItems = footerItems.slice(3, 6);
 
+  const { features } = data.section2;
+  const { articles } = data;
+
   return (
     <div>
       <header>
@@ -57,18 +60,14 @@ function App() {
             ))}
           </ul>
         </nav>
+        <button class="request-button hidden">Request Invite</button>
       </header>
       <main>
         <section className="section1">
           <div className="image-wrapper">
-            {/* <img
-              src="assets/bg-intro-mobile.svg"
-              alt="background image"
-              className="bg-image"
-            /> */}
             <img
               src="assets/image-mockups.png"
-              alt="image-mockups"
+              alt="mockups"
               className={`overlay-image ${isMenuOpen ? "backdrop" : ""}`}
             />
           </div>
@@ -88,108 +87,56 @@ function App() {
           <h2>Why choose Easybank?</h2>
           <p>
             We leverage Open Banking to turn your bank account into your
-            financial hub.Control your finances like never before.
+            financial hub. Control your finances like never before.
           </p>
           <div className="features">
-            <div>
-              <img src="assets/icon-online.svg" alt="Online Banking Icon" />
-              <h3 class="feature-heading">Online Banking</h3>
-              <p class="feature-discription">
-                Our modern web and mobile applications allow you to keep track
-                of your finances wherever you are in the world.
-              </p>
-            </div>
-            <div>
-              <img src="assets/icon-budgeting.svg" alt="Budgeting Icon" />
-              <h3 class="feature-heading">Simple Budgeting</h3>
-              <p class="feature-discription">
-                See exactly where your money goes each month. Receive
-                notifications when you’re close to hitting your limits.
-              </p>
-            </div>
-            <div>
-              <img src="assets/icon-onboarding.svg" alt="Onboarding Icon" />
-              <h3 class="feature-heading">Fast Onboarding</h3>
-              <p class="feature-discription">
-                We don’t do branches. Open your account in minutes online and
-                start taking control of your finances right away.
-              </p>
-            </div>
-            <div>
-              <img src="assets/icon-api.svg" alt="API Integration Icon" />
-              <h3 class="feature-heading">Open API</h3>
-              <p class="feature-discription">
-                Manage your savings, investments, pension, and much more from
-                one account. Tracking your money has never been easier.
-              </p>
-            </div>
+            {features.map((feature, index) => (
+              <div key={index}>
+                <img src={feature.icon} alt={feature.heading} />
+                <h3 className="feature-heading">{feature.heading}</h3>
+                <p className="feature-description">{feature.description}</p>
+              </div>
+            ))}
           </div>
         </section>
 
         <article>
           <h2>Latest Articles</h2>
           <div className="articles-container">
-            <div>
-              <img src="assets/image-currency.jpg" alt="Currency exchange" />
-              <div className="article-details">
-                <p class="article-author">By Claire Robinson</p>
-                <h3>Receive money in any currency with no fees</h3>
-                <p>
-                  The world is getting smaller and we’re becoming more mobile.
-                  So why should you be forced to only receive money in a single
-                  …
-                </p>
+            {articles.map((article, index) => (
+              <div key={index}>
+                <img
+                  src={article.image}
+                  alt={article.title}
+                  className="article-image"
+                />
+                <div className="article-details">
+                  <p className="article-author">{article.author}</p>
+                  <h3>{article.title}</h3>
+                  <p>{article.content}</p>
+                </div>
               </div>
-            </div>
-            <div>
-              <img src="assets/image-restaurant.jpg" alt="Restaurant dining" />
-              <div className="article-details">
-                <p class="article-author">By Wilson Hutton</p>
-                <h3>Treat yourself without worrying about money</h3>
-                <p>
-                  Our simple budgeting feature allows you to separate out your
-                  spending and set realistic limits each month. That means you …
-                </p>
-              </div>
-            </div>
-            <div>
-              <img src="assets/image-plane.jpg" alt="Airplane travel" />
-              <div className="article-details">
-                <p class="article-author">By Wilson Hutton</p>
-                <h3>Take your Easybank card wherever you go</h3>
-                <p>
-                  We want you to enjoy your travels. This is why we don’t charge
-                  any fees on purchases while you’re abroad. We’ll even show you
-                  …
-                </p>
-              </div>
-            </div>
-            <div>
-              <img src="assets/image-confetti.jpg" alt="Confetti celebration" />
-              <div className="article-details">
-                <p class="article-author">By Claire Robinson</p>
-                <h3>Our invite-only Beta accounts are now live!</h3>
-                <p>
-                  After a lot of hard work by the whole team, we’re excited to
-                  launch our closed beta. It’s easy to request an invite through
-                  the site ...
-                </p>
-              </div>
-            </div>
+            ))}
           </div>
         </article>
       </main>
+
       <footer>
-        <img src={whitelogo} alt="easybank logo" className="footer-logo" />
-        <div className="social-media">
-          {mediaIcons.map((icon, index) => (
-            <div
-              key={index}
-              dangerouslySetInnerHTML={{ __html: icon }}
-              className="media-icon"
-            />
-          ))}
+        <div>
+          <img src={whitelogo} alt="easybank logo" className="footer-logo" />
+          <div className="social-media">
+            {mediaIcons.map((icon, index) => (
+              <div
+                key={index}
+                tabIndex={0} 
+                role="button"
+                dangerouslySetInnerHTML={{ __html: icon }}
+                className="media-icon"
+              />
+            ))}<span className="sr-only">{icon}</span>
+          </div>
         </div>
+
         <div className="footer-wrapper">
           <div className="left">
             <ul>
@@ -210,8 +157,12 @@ function App() {
             </ul>
           </div>
         </div>
-        <button class="request-button">Request Invite</button>
-        <div className="attribution">&copy; Easybank. All Rights Reserved</div>
+        <div className="att-wrapper">
+          <button class="request-button">Request Invite</button>
+          <div className="attribution">
+            &copy; Easybank. All Rights Reserved
+          </div>
+        </div>
       </footer>
     </div>
   );
